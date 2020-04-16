03196ebec056cc44f3e5b377669981b99bd1e277
Âlessandro <asovitorio@gmail.com>
2020-04-16 19:07:44 -0300

    Arrumando as depedencias

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
 const config = {
     host: "localhost", 
-    port: 3306,
     username: "root",
     password: "admin",
     database: "fake-instagram",
     dialect: "mysql"
+    
 } 
 
+module.exports = config;
--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,14 +1,16 @@
+const sequelize = require('sequelize');
+const dbConfig = require('../config/database');
+const connection = new sequelize(dbConfig);
 
 const usuarioController= {
     login : (req, res) => {
 
-        // res.send("deu certo")
-       
+             
         res.render('auth/login', { title: 'Express' })
     },
     register: (req,res) => {
 
-        res.render('auth/register', { title: 'Express' })
+        res.render('auth/register', {title:'Express'})
     
 }
 
